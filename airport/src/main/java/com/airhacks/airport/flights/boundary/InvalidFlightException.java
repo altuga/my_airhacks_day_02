7af59017ed4b13cb847a656daa95e7e6731a3d28
Adam Bien <abien@adam-bien.com>
2019-04-03 14:22:42 +0200

    Exceptions map themselves

--- airport/src/main/java/com/airhacks/airport/flights/boundary/InvalidFlightException.java
+++ airport/src/main/java/com/airhacks/airport/flights/boundary/InvalidFlightException.java
@@ -1,14 +1,19 @@
 
 package com.airhacks.airport.flights.boundary;
 
+import javax.ejb.ApplicationException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author airhacks.com
  */
-public class InvalidFlightException extends RuntimeException {
+@ApplicationException(rollback = true)
+public class InvalidFlightException extends WebApplicationException {
 
     public InvalidFlightException(String message) {
-        super(message);
+        super(Response.status(400).header("info", message).build());
     }
 
 }
